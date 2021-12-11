ccce74f214833f192f437c1e589d3f4e42bf6757
Ben <90007599+mrbce@users.noreply.github.com>
2021-12-11 16:14:49 +0000

    Update code.js

--- code.js
+++ code.js
@@ -2,3 +2,8 @@
 const myAge = 24;
 // This variable will change
 let earlyYears = 2;
+earlyYears *= 10.5;
+// The below is counteracting the 2 from earlyYears
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
